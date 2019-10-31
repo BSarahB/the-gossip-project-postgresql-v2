d76bfb51311d79974a7b2f0fe397618d92b1b201
BSarahB <bslajta@gmail.com>
2019-10-31 04:31:19 +0100

    testing params fromulaire comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,8 +10,14 @@ class CommentsController < ApplicationController
 
 
      # Méthode qui créé un potin à partir du contenu du formulaire de new.html.erb, soumis par l'utilisateur
-    # pour info, le contenu de ce formulaire sera accessible dans le hash params (ton meilleur pote)
+     # pour info, le contenu de ce formulaire sera accessible dans le hash params (ton meilleur pote)
     # Une fois la création faite, on redirige généralement vers la méthode show (pour afficher le potin créé)
+	#on verifie que le formulaire envoie bien les params saisis code entre les puts $ non necessaire , mais qui nous permet de checker au debut 
+   	  puts "*" * 100
+   	  puts "ceci est le contenu de params formulaire comments et signifie que le formulaire pointe bien sur create :"
+      puts params
+      puts "*" * 100
+   	 #OK ca envoie bien ici 
 
 
         @comment = Comment.new(content: params[:content], user_id: 3, gossip_id: params[:gossip_id])
