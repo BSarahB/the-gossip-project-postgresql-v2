e0c579bd1958337533556e5e2caceff0de84ebc7
BSarahB <bslajta@gmail.com>
2019-11-01 14:21:02 +0100

    restore previous code

--- app/helpers/gossips_helper.rb
+++ app/helpers/gossips_helper.rb
@@ -1,4 +1,5 @@
 module GossipsHelper
+	#pas necessaire ici je crois mais je le laisse au cas ou...flemme de tester fatigue, necessaire sure dans sessions_helper.erb
 	def current_user
     User.find_by(id: session[:user_id])
   end
