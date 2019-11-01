142b6ad93e98a902d02137c2b23ccd409970a538
BSarahB <55717377+BSarahB@users.noreply.github.com>
2019-11-01 09:54:31 +0100

    adding file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'gossips#index'
 
-
   resources :welcome  # notre ancien get 'welcome/:first_name' on a remplace :first_name par :id dans le controller, et la view, to: 'welcome#show' #route dynamique vers l URL cachee
   resources :team
   resources :contact
